51c2990c46d89709e63e20742dfbc25bd191b46a
anujm <aanujmaurya8127@gmail.com>
2026-03-07 14:43:58 +0530

    This is cart Repository to fetch the data from database and this repo has some extra method to fetch the data from database

---
+++ src/main/java/com/foodie/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package com.foodie.repository;
+
+import com.foodie.entity.CartEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<CartEntity, String> {
+}
