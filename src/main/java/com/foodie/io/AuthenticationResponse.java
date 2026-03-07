0f4445f73065327957fe230051b13d8681d3ba07
anujm <aanujmaurya8127@gmail.com>
2026-03-07 14:42:30 +0530

    This class is manage the authenticationRequest and AuthenticationResponse this is type of DTO Class

---
+++ src/main/java/com/foodie/io/AuthenticationResponse.java
@@ -0,0 +1,12 @@
+package com.foodie.io;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+
+public class AuthenticationResponse {
+    private String email;
+    private String token;
+}
