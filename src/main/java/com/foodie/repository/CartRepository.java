bd37709d0199f0abce50f883b8bc349152c15217
anujm <aanujmaurya8127@gmail.com>
2026-03-07 17:11:09 +0530

    Cart Repo is used to fetch the data from the database and in this repo used some custom method to call the items from the database

--- src/main/java/com/foodie/repository/CartRepository.java
+++ src/main/java/com/foodie/repository/CartRepository.java
@@ -4,6 +4,12 @@ import com.foodie.entity.CartEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CartRepository extends JpaRepository<CartEntity, String> {
+
+    Optional<CartEntity> findByUserId(String userId);
+
+    void deleteByUserId(String userId);
 }
