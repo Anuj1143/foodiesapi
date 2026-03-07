9383d882be698eb174651c5bdbe9b62e7a06e6f4
anujm <aanujmaurya8127@gmail.com>
2026-03-07 17:10:06 +0530

    this is cart controller class to manage all the end points related to cart items

--- src/main/java/com/foodie/controller/CartController.java
+++ src/main/java/com/foodie/controller/CartController.java
@@ -1,9 +1,48 @@
 package com.foodie.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.foodie.io.CartRequest;
+import com.foodie.io.CartResponse;
+import com.foodie.services.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/cart")
+@RequiredArgsConstructor
 public class CartController {
+
+    private final CartService cartService;
+    @PostMapping
+    public CartResponse addToCart(@RequestBody CartRequest request){
+       String foodId= request.getFoodId();
+       if(foodId==null || foodId.isEmpty()){
+           throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Food Id not Found");
+       }
+       return cartService.addToCart(request);
+
+    }
+
+    @GetMapping()
+    public CartResponse getCart(){
+      return cartService.getCart();
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void clearCart(){
+        cartService.clearCart();
+    }
+    @PostMapping("/remove")
+    public CartResponse removeFromCart(@RequestBody CartRequest request){
+        String foodId=request.getFoodId();
+        if(foodId ==null || foodId.isEmpty()){
+            throw  new ResponseStatusException(HttpStatus.BAD_REQUEST, "Food not Found");
+        }
+       return cartService.removeFromCart(request);
+    }
 }
