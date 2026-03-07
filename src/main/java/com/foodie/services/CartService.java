a6d0a42b598b13a253596172388fdaa1be296684
anujm <aanujmaurya8127@gmail.com>
2026-03-07 17:12:02 +0530

    this is cart service interface and class where all the function present related to cart

--- src/main/java/com/foodie/services/CartService.java
+++ src/main/java/com/foodie/services/CartService.java
@@ -1,4 +1,15 @@
 package com.foodie.services;
 
+import com.foodie.io.CartRequest;
+import com.foodie.io.CartResponse;
+
 public interface CartService {
+
+   CartResponse addToCart(CartRequest request);
+
+   CartResponse getCart();
+
+   void clearCart();
+
+  CartResponse removeFromCart(CartRequest cartRequest);
 }
